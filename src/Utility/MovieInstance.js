0ff24485cf936fd10d0e8438b254f67b2938ef8c
daniyacodes~ <daniyaasefa01@gmail.com>
2026-08-27 07:30:02 +0300

    added comment about axios instance

--- src/Utility/MovieInstance.js
+++ src/Utility/MovieInstance.js
@@ -3,5 +3,5 @@ import axios from "axios"
 //configuration eyeseran new
 let movieInstance = axios.create({
     baseURL:"https://api.themoviedb.org/3"
-})
+})//axios.create() returns an object/function with Axios methods and configuration attached to it ... axios.create() is the API Axios give us for creating an independently configured instance ... configured Axios instance = an Axios request-making object with its own default settings
 export default movieInstance
